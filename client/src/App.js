b8babd9876cd532926af687c341d8df56557e72d
James McGauran <jamesmcgauran11@gmail.com>
2021-11-28 15:22:49 +0000

    Change route

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import { Fragment } from 'react';
 import Splash from './components/Splash';
-import Taleb from './components/Taleb'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
-
 
 // Constants
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 function App() {
-
-   // Render Methods
+  // Render Methods
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button className='cta-button connect-wallet-button'>
       Connect to Wallet
     </button>
   );
@@ -23,12 +20,9 @@ function App() {
   return (
     <Router>
       <Fragment>
-          <Routes>
-          <Route exact path='/' element={<Splash/>} />
-          </Routes>
-          <Routes>
-          <Route exact path='/taleb' element={<Taleb/>} />
-          </Routes>
+        <Routes>
+          <Route exact path='/duppy_NFT' element={<Splash />} />
+        </Routes>
       </Fragment>
     </Router>
   );
